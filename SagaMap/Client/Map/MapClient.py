373433cf6c346bd570a0bb76660ada0344f6e3e3
aroncds <aronxgames@yahoo.com.br>
2015-08-22 23:05:07 -0300

    Começa o processo de criação de models para o banco de dados.

--- SagaMap/Client/Map/MapClient.py
+++ SagaMap/Client/Map/MapClient.py
@@ -4,6 +4,7 @@ from Packets.Map.World.Set.SendStart import SendStart
 
 def OnIdentify(pck, client):
 	charID = pck.getCharID()
+	print "Char: " + str(charID)
 	client.sessionID = pck.getGatewaySessionID()
 
 	sendStart = SendStart()
@@ -11,3 +12,10 @@ def OnIdentify(pck, client):
 	sendStart.setLocation(0.0,0.0,0.0)
 
 	client.sendPacket(sendStart)
+
+
+from DB.model import Model
+
+class Char(model.Model):
+	charID = model.AutoField()
+	name = model.CharField({"max_length": 100})
---
+++ SagaMap/DB/model.py
@@ -0,0 +1,78 @@
+class Field(object):
+	null=False
+	blank=False
+	auto_increment=False
+	type_field = 'char'
+	_field_db = True
+
+
+class CharField(Field):
+	max_length = 50
+	type_field = 'char'
+
+
+class IntField(Field):
+	type_field = 'int'
+
+
+class FloatField(Field):
+	type_field = 'float'
+
+
+class AutoID(IntField):
+	auto_increment = True
+
+
+class Model(object):
+	pk = Field
+	table = 'Char'
+	fields = {}
+
+	def __init__(self):
+		self.__empty_instance()
+
+	def save(self, connection=None):
+		query = self.__update__()
+
+	def __empty_instance(self):
+		lista= dir(self)
+		for key in lista:
+			prop = getattr(self, key, None)
+			if(hasattr(prop, '_field_db')):
+				self.fields[key] = prop
+				setattr(self, key, None)
+
+	def __query__select(self):
+		query = "SELECT * FROM WHERE id=%s"
+
+	def __insert__(self):
+		fields = []
+		values = []
+
+		for key in self.fields.keys():
+			v = getattr(self, key, '')
+			fields.append("`%s`," % key)
+			values.append("'%s'," % v)
+
+		fields = "".join(fields)[:-1]
+		values = "".join(values)[:-1]
+
+		return "INSERT INTO " + self.table + " (%s) VALUES (%s);" % (fields, values)
+
+	def __update__(self):
+		fields = []
+
+		for key in self.fields.keys():
+			v = getattr(self, key, '')
+			fields.append(" `%s`='%s'," % (key, v))
+
+		fields = "".join(fields)[:-1]
+
+		query = "UPDATE " + self.table + " SET %s WHERE id='%s';" % (fields, self.pk)
+
+		return query
+		
+
+	def delete(self):
+		query = "DELETE FROM " + self.table + " WHERE id='%s'" % self.pk
+		
