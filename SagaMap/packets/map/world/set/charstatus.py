5768d25d2df1a54a738db1c5680b2634f95e0636
Aron de Castro <aronxgames@yahoo.com.br>
2015-08-27 00:59:21 -0300

    Adiciona pacote charstatus

---
+++ SagaMap/packets/map/world/set/charstatus.py
@@ -0,0 +1,35 @@
+from packet import Packet
+
+
+class CharStatus(Packet):
+	def __init__(self):
+		self.data = [0] * 35
+		self.setID(0x0308)
+		self.size = 35
+		self.setLength()
+
+	def setJob(self, job):
+		self.data[12] = job
+
+	def setExp(self, cExp, jExp):
+		self.setUInt(8, cExp)
+		self.setUInt(13, jExp)
+
+	def setHP(self, current, max):
+		self.setUShort(17, current)
+		self.setUShort(19, max)
+
+	def setSP(self, current, max):
+		self.setUShort(21, current)
+		self.setUShort(23, max)
+
+	def setLC(self, current, max):
+		self.data[25] = current
+		self.data[26] = max
+
+	def setLP(self, current, max):
+		self.data[27] = current
+		self.data[28] = max
+
+	def setVisibleField(self, u):
+		self.setUShort(34, u)
