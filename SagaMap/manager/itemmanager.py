b5efa4cac7e4de497308953f4e89aa662023e3fb
Aron de Castro <aronxgames@yahoo.com.br>
2016-01-08 17:34:55 -0200

    Corrige load de itens

--- SagaMap/manager/itemmanager.py
+++ SagaMap/manager/itemmanager.py
@@ -5,7 +5,10 @@ from db.models.item import Item
 import xml.etree.ElementTree as ET
 
 
-class ManagerLoadItens(dict):
+class ManagerLoadItens(object):
+    def __init__(self):
+        self.lista = None
+
     def create_item(self, lista):
         item = None
         for itemXML in lista:
@@ -18,7 +21,9 @@ class ManagerLoadItens(dict):
         with ET.parse("../DB/itemDB.xml") as tree:
             root = tree.getroot()
             lista = root.getchildren()
-            self = {item.id: item for item in self.create_item(lista)}
+            self.lista = {item.id: item for item in self.create_item(lista)}
 
-_loaded_itens = ManagerLoadItens().load_item_data()
-items = weakref.proxy(_loaded_itens)
+_loaded_itens = ManagerLoadItens()
+_loaded_itens.load_item_data()
+
+items = weakref.proxy(_loaded_itens.lista)
