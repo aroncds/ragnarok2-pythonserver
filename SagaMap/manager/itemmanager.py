1967d181732d2bc48ed42c6caee1364e4f79b675
Aron de Castro <aronxgames@yahoo.com.br>
2016-01-08 17:31:11 -0200

    Melhora codigo para load de itens

--- SagaMap/manager/itemmanager.py
+++ SagaMap/manager/itemmanager.py
@@ -4,17 +4,21 @@ from db.models.item import Item
 
 import xml.etree.ElementTree as ET
 
-_items = {}
-items = weakref.proxy(_items)
 
-
-def load_item_data():
-    with ET.parse("../DB/itemDB.xml") as tree:
-        root = tree.getroot()
-        lista = root.getchildren()
-
+class ManagerLoadItens(dict):
+    def create_item(self, lista):
+        item = None
         for itemXML in lista:
             item = Item()
             fields = itemXML.getchildren()
             item.set_values(fields)
-            _items[item.id] = item
+            yield item
+
+    def load_item_data(self):
+        with ET.parse("../DB/itemDB.xml") as tree:
+            root = tree.getroot()
+            lista = root.getchildren()
+            self = {item.id: item for item in self.create_item(lista)}
+
+_loaded_itens = ManagerLoadItens().load_item_data()
+items = weakref.proxy(_loaded_itens)
