74e51f9535112b070aac2aa5609c1cc65a1a93ac
Aron Castro <aronxgames@yahoo.com.br>
2016-01-12 00:08:42 -0200

    Corrige loginclient para varias tentativas de conexão

--- SagaMap/client/login/loginsession.py
+++ SagaMap/client/login/loginsession.py
@@ -9,8 +9,20 @@ from settings import LOGIN_HOST, LOGIN_PORT
 
 class LoginClient(Client, Thread):
         def __init__(self):
+            import time
+            running = True
             self.connection = socket(AF_INET, SOCK_STREAM)
-            self.connection.connect(LOGIN_HOST, LOGIN_PORT)
+
+            while running:
+                try:
+                    self.connection.connect((LOGIN_HOST, LOGIN_PORT))
+                    running = False
+                except Exception as e:
+                    print("#---Login Server: Failed---#")
+                    print(e)
+
+                    running = True
+                    time.sleep(5)
 
         def run(self):
             while 1:
